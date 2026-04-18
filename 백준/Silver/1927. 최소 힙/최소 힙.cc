617da876abae73c0b6ae46b8f7e03d7af3cd1ff9
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-04-18 17:49:40 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct cmp {
+    bool operator()(int a, int b) {
+        return a > b;
+    }
+};
+
+int main() {
+
+    /**
+     * 최소 힙
+     * https://www.acmicpc.net/problem/1927
+     * 
+     * x가 주어졌을 때 
+     * 0이 주어지면 가장 작은 값을 출력 및 배열에서 제거 하고,
+     * 0이 아닌 값이 주어지면 배열에 추가하는 문제
+     * 
+     * -- 자연어 풀이 --
+     * 1. n을 입력 받는다
+     * 2. n번 반복하며 값을 받을 때 0이 주어지면 가장 작은 값을 출력 및 제거하고,
+     *    0이 아닌 값이 주어지면 배열에 추가한다
+     * 
+     */
+
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, cmp> pq;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        if (x) {
+            pq.push(x);
+        } else {
+            if (pq.empty()) {
+                cout << "0\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+}
