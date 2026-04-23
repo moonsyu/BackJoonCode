0da69648ace7f821e32491bbe3cf6cb8d12fa7d6
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-04-23 20:49:10 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,47 +1,42 @@
-#include<iostream>
-#include<algorithm>
-#include<deque>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int answer = 999999;
-
-void min_count(deque<int> &save, long long n) {
-	if (save.size() < answer) {
-		if (n == 1) {
-			if (answer > save.size()) {
-				answer = save.size();
-			}
-		}
-		else {
-			if (n % 3 == 0) {
-				save.push_back(3);
-				min_count(save, n / 3);
-				save.pop_back();
-			}
-			if (n % 2 == 0) {
-				save.push_back(2);
-				min_count(save, n / 2);
-				save.pop_back();
-			}
-
-			save.push_back(1);
-			min_count(save, n - 1);
-			save.pop_back();
-
-		}
-	}
-}
-
 int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-	cout.tie(NULL);
+	/**
+	* 정수 x가 주어졌을 때 1로 만드는 최소한의 연산 횟수를 구하라
+	* 
+	* 연산 방법
+	* 1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
+	* 2. X가 2로 나누어 떨어지면, 2로 나눈다.
+	* 3. 1을 뺀다.
+	* 
+	* -- 자연어 풀이 --
+	* 1. n을 입력 받는다
+	* 2. n + 1 사이즈의 1차원 dp를 만든다
+	* 3. dp[0] = 0; dp[1] = 0; dp[2] = 1; dp[3] = 1;로 초기화 한다
+	* 4. 4 ~ n까지 반복하며 min(dp[i/3], dp[i/2], dp[i-1]) + 1을 계산한다
+	* 5. dp[n] 출력한다
+	*/
 
-	long long n;
-	deque<int> save;
+
+	int n;
 	cin >> n;
 
-	min_count(save, n);
-	cout << answer;
+	vector<long long> dp(n + 1, 0);
+	if(n >= 2) 
+		dp[2] = 1;
+	if(n >= 3) 
+		dp[3] = 1;
+
+	for (int i = 4; i <= n; ++i) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0) 
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		if (i % 3 == 0) 
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+	}
+
+	cout << dp[n];
 }
