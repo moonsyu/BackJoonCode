a0c3ab26adbfd317a84fd9ce52361007cfb02ea9
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-04-27 15:27:13 +0900

    [D3] Title: 0/1 Knapsack, Time: 19 ms, Memory: 5,832 KB -BaekjoonHub

---
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		int n, k;
+		cin >> n >> k;
+
+		vector<int> v(n + 1);
+		vector<int> c(n + 1);
+		for (int j = 1; j <= n; j++) {
+			cin >> v[j] >> c[j];
+		}
+
+		vector<int> dp(k + 1);
+		for (int j = 1; j <= n; j++) {
+			for (int l = k; l >= 0; l--) {
+				if (l >= v[j]) {
+					dp[l] = max(dp[l], dp[l - v[j]] + c[j]);
+				}
+			}
+		}
+
+		cout << "#" << i + 1 << " " << dp[k] << endl;
+	}
+}
