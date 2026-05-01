27a131b19b3869b27b2ef767b054b257f7e5a36a
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-01 09:54:04 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 72 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.cpp
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.cpp
@@ -2,8 +2,12 @@
 #include <vector>
 #include <algorithm>
 
+#define item_cnt 2
+
 using namespace std;
 
+long long answer;
+long long limit;
 
 /* 내림차순 정렬 */
 struct cmp {
@@ -11,6 +15,23 @@ struct cmp {
 		return a > b;
 	}
 };
+
+
+/* item_cnt 만큼 물건 선택 후 합 계산 */
+void dfs(vector<long long>& a, int idx, int cnt, long long sum) {
+
+	// 물건을 item_cnt 만큼 선택한 경우
+	if (cnt == item_cnt) {
+		answer = max(answer, sum);
+		return;
+	}
+
+	for (int i = idx; i < a.size(); i++) {
+		if (sum + a[i] <= limit) {
+			dfs(a, i + 1, cnt + 1, sum + a[i]);
+		}
+	}
+}
 
 
 int main() {
@@ -26,17 +47,22 @@ int main() {
 			cin >> a[j];
 		}
 
-		// 내림차순 정렬
-		sort(a.begin(), a.end(), cmp());
+		//// 내림차순 정렬
+		//sort(a.begin(), a.end(), cmp());
 
-		long long answer = -1;
-		for (int j = 0; j < n; j++) {
-			for (int k = j + 1; k < n; k++) {
-				if (a[j] + a[k] <= m) {
-					answer = max(answer, a[j] + a[k]);
-				}
-			}
-		}
+		//long long answer = -1;
+		//for (int j = 0; j < n; j++) {
+		//	for (int k = j + 1; k < n; k++) {
+		//		if (a[j] + a[k] <= m) {
+		//			answer = max(answer, a[j] + a[k]);
+		//		}
+		//	}
+		//}
+
+
+		answer = -1;
+		limit = m;
+		dfs(a, 0, 0, 0);
 
 		cout << "#" << i + 1 << " " << answer << "\n";
 	}
