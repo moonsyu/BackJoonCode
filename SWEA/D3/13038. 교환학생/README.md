9913f776432b0eb9e79c9f0a564ab4f15ed692ee
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-05 20:49:37 +0900

    [D3] Title: 교환학생, Time: 543 ms, Memory: 5,832 KB -BaekjoonHub

---
+++ SWEA/D3/13038. 교환학생/README.md
@@ -0,0 +1,15 @@
+# [D3] 교환학생 - 13038 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXxNn6GaPW4DFASZ) 
+
+### 성능 요약
+
+메모리: 5,832 KB, 시간: 543 ms, 코드길이: 881 Bytes
+
+### 제출 일자
+
+2026-05-05 20:49
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/13038. 교환학생/교환학생.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+
+#define week_size 7
+
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		int n;
+		cin >> n;
+
+		// 일주일 강의 계획
+		vector<int> plan(week_size);
+		for (int j = 0; j < week_size; j++) {
+			cin >> plan[j];
+		}
+
+		// 강의 시작일 및 1주간 강의 개수 조회
+		int start_day = -1;
+		int class_cnt = 0;
+		for (int j = 0; j < week_size; j++) {
+			if (plan[j]) {
+				if (start_day == -1) {
+					start_day = j;
+				}
+
+				class_cnt++;
+			}
+		}
+
+		long long answer = LLONG_MAX;
+		for (int j = start_day; j < week_size; j++) {
+			long long day_cnt = 0;
+			int copy_n = n;
+			for (int k = j; copy_n > 0; k++) {
+				day_cnt++;
+
+				if (plan[k % week_size]) {
+					copy_n--;
+				}
+			}
+
+			answer = min(answer, day_cnt);
+		}
+
+		cout << "#" << i + 1 << " " << answer << "\n";
+	}
+}
