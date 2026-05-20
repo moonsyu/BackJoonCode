c00be8f409ac8f8f6ec927e5935c238edb942f54
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-20 19:41:30 +0900

    [D3] Title: 유치원생은 쉽게 푸는 문제, Time: 13 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/25655. 유치원생은 쉽게 푸는 문제/유치원생은 쉽게 푸는 문제.cpp
+++ SWEA/D3/25655. 유치원생은 쉽게 푸는 문제/유치원생은 쉽게 푸는 문제.cpp
@@ -16,21 +16,22 @@ int main() {
 		cin >> n;
 
 		string answer = "";
-		if (n == 1) {
-			answer = "0";
-		}
-		else if (n % 2){
-			answer += "4";
-			for (int i = 0; i < n / 2; i++) {
-				answer += "8";
+		// n이 홀수일 때
+		if (n % 2) {
+			// 1이라면 0,
+			// 그 외 4로 시작
+			if (n == 1) {
+				answer = "0";
+			} else {
+				answer = "4";
 			}
 		}
-		else {
-			for (int i = 0; i < n / 2; i++) {
-				answer += "8";
-			}
+
+		// 나머지 부분 8로 채우기
+		for (int i = 0; i < n / 2; i++) {
+			answer += "8";
 		}
-	
+
 		cout << answer << endl;
 	}
 }
