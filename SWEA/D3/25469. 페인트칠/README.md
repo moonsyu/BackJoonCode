a63ae8f2476ea66d00689506728d1bec19520c68
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-05-20 19:53:17 +0900

    [D3] Title: 페인트칠, Time: 12 ms, Memory: 5,972 KB -BaekjoonHub

---
+++ SWEA/D3/25469. 페인트칠/README.md
@@ -0,0 +1,15 @@
+# [D3] 페인트칠 - 25469 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZlP2QbavInHBIO0) 
+
+### 성능 요약
+
+메모리: 5,972 KB, 시간: 12 ms, 코드길이: 1,293 Bytes
+
+### 제출 일자
+
+2026-05-20 19:53
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/25469. 페인트칠/페인트칠.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/* 가로 행 방향 색칠 횟수 반환 */
+int row_cnt(vector<vector<string>>& board) {
+
+	int result = 0;
+	for (int i = 0; i < board.size(); i++) {
+		
+		bool check = true;
+		for (int j = 0; j < board[i].size() && check; j++) {
+			if (board[i][j] != "#") {
+				check = false;
+			}
+		}
+
+		if (check) {
+			result++;
+		}
+	}
+
+	return result;
+}
+
+
+/* 세로 열 방향 색칠 횟수 반환 */
+int col_cnt(vector<vector<string>>& board) {
+
+	int result = 0;
+	for (int i = 0; i < board[0].size(); i++) {
+
+		bool check = true;
+		for (int j = 0; j < board.size() && check; j++) {
+			if (board[j][i] != "#") {
+				check = false;
+			}
+		}
+
+		if (check) {
+			result++;
+		}
+	}
+
+	return result;
+}
+
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	for (int t = 1; t <= tc; t++) {
+		int h, w;
+		cin >> h >> w;
+
+		vector<vector<string>> board(h, vector<string>(w));
+		for (int i = 0; i < h; i++) {
+			string input;
+			cin >> input;
+
+			for (int j = 0; j < w; j++) {
+				board[i][j] = input[j];
+			}
+		}
+
+		// 가로 세로 방향으로 칠한 횟수 세기
+		int answer = 0;
+		answer += row_cnt(board);
+		answer += col_cnt(board);
+
+		// 만약 모든 칸이 칠해져 있다면
+		if (answer == h + w) {
+			// 더 작은 방향으로 갱신
+			answer = min(h, w);
+		}
+
+		cout << answer << endl;
+	}
+}
