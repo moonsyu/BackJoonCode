1a44f6d9282c5306568b39726695dd49e92f40fa
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-01 18:32:44 +0900

    [D2] Title: 숫자 배열 회전, Time: 5 ms, Memory: 404 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.cpp
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.cpp
@@ -1,52 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+
 int main() {
-    /** 
-     * N * N 크기의 2차원 배열이 주어질 때,
-     * 90도, 180도, 270도 회전한 배열을 출력하라
-     * 
-     * -- 자연어 풀이 --
-     * 1. N을 입력 받는다
-     * 2. N * N 크기의 2차원 배열을 입력 받는다
-     * 3. j, k로 2차원 배열을 순회한다
-     * 4. 90도는 (n - k - 1, j)를 출력하고
-     * 5. 180도는 (n - j - 1, n - k - 1)을 출력하고 
-     * 6. 270도는 (k, n - j - 1)를 출력한다
-     */
 
-    int tc;
-    cin >> tc;
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
 
-    for (int i = 0; i < tc; i++) {
+    int t;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; tc++) {
         int n;
         cin >> n;
 
-        vector<vector<int>> board(n, vector<int>(n));
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                cin >> board[j][k];
+        vector<vector<int>> arr(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> arr[i][j];
             }
         }
 
-        cout << "#" << i + 1 << "\n";
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                cout << board[n - k - 1][j];
+        cout << "#" << tc << "\n";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << arr[n - 1 - j][i];
             }
-
             cout << " ";
-            for (int k = 0; k < n; k++) {
-                cout << board[n - j - 1][n - k - 1];
-            }
 
-            cout << " ";
-            for (int k = 0; k < n; k++) {
-                cout << board[k][n - j - 1];
+            for (int j = 0; j < n; j++) {
+                cout << arr[n - 1 - i][n - 1 - j];
             }
-            
+            cout << " ";
+
+            for (int j = 0; j < n; j++) {
+                cout << arr[j][n - 1 - i];
+            }
             cout << "\n";
         }
     }
