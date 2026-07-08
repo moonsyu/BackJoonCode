d8010b1557fd87b9c93a3d40c54e1c79c88ceec1
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-09 08:10:26 +0900

    [level 2] Title: 숫자의 표현, Time: 0.06 ms, Memory: 4.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for (int i = 1; i <=n; i++) {
+        int num = i;
+        int sum = 0;
+        while (sum < n) {
+            sum += num++;
+            
+            if (sum == n) {
+                answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
