7d29325384818c6c261b415094e2ddfae313c439
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-10 14:15:00 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 4.53 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int solution(int n)
 {
     int ans = 0;
-    while (n > 0) {
-        if (n % 2) {
-            ans++;
-            n -= 1;
-        } else {
-            n /= 2;
-        }
-    }
     
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
     return ans;
 }
