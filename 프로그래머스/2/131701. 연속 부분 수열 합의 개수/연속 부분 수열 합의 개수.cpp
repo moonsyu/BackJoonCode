a2a8e151023b7931b9beb00c6f07f0a9b45840d8
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-10 17:28:58 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1610.78 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -4,9 +4,10 @@
 
 using namespace std;
 
-int solution(vector<int> elements) {
-    int answer = 0;
-    
+
+/* 메모리 절약 버전 */
+int memory_save(vector<int> &elements) {
+    int result = 0;
     
     vector<int> calc;
     for (int i = 1; i <= elements.size(); i++) {
@@ -25,10 +26,43 @@ int solution(vector<int> elements) {
     int num = 0;
     for (int i = 0; i < calc.size(); i++) {
         if (num != calc[i]) {
-            answer++;
+            result++;
             num = calc[i];
         }
     }
     
+    return result;
+}
+
+
+/* 시간 절약 버전 */
+int time_save(vector<int> &elements) {
+    int result = 0;
+    
+    vector<bool> calc(1000001, true);
+    for (int i = 1; i <= elements.size(); i++) {
+        for (int j = 0; j < elements.size(); j++) {
+            int sum = 0;
+            for (int k = j; k < j + i; k++) {
+                sum += elements[k % elements.size()];
+            }
+            
+            if (calc[sum]) {
+                result++;
+                calc[sum] = false;
+            }
+        }
+    }
+    
+    return result;
+}
+
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    
+    
+    // answer = memory_save(elements); // 메모리 절약
+    answer = time_save(elements); // 시간 절약
     return answer;
 }
