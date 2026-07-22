966f7b338dbcd32018ab4c7fcac7d20505226501
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-22 15:14:49 +0900

    [D4] Title: 치즈 도둑, Time: 314 ms, Memory: 9,944 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.cpp
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 int n;
 int cheese[100][100];
-int visited[100][100];
+int visited[100][100][100];
 int day_list[101];
 
 queue<pair<int, int>> que;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-int cheese_block(int day) {
+int cheese_block(int day, int t) {
     int result = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (cheese[i][j] > day && visited[i][j] != day) {
+            if (cheese[i][j] > day && visited[i][j][t] != day) {
                 result++;
 
                 que.push({ i, j });
-                visited[i][j] = day;
+                visited[i][j][t] = day;
 
                 while (!que.empty()) {
                     auto [x, y] = que.front();
@@ -35,8 +35,8 @@ int cheese_block(int day) {
                             continue;
                         }
 
-                        if (cheese[nx][ny] > day && visited[nx][ny] != day) {
-                            visited[nx][ny] = day;
+                        if (cheese[nx][ny] > day && visited[nx][ny][t] != day) {
+                            visited[nx][ny][t] = day;
                             que.push({ nx, ny });
                         }
                     }
@@ -58,7 +58,6 @@ int main() {
     cin >> tc;
 
     for (int t = 1; t <= tc; t++) {
-        memset(visited, 0, sizeof(visited));
 
         cin >> n;
         for (int i = 0; i < n; i++) {
@@ -71,7 +70,7 @@ int main() {
         int answer = 1;
         for (int day = 1; day < 100; day++) {
 			if (day_list[day] == t) {
-                answer = max(answer, cheese_block(day));
+                answer = max(answer, cheese_block(day, t));
 			}
         }
 
