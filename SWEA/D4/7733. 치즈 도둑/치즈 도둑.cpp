f6b2c6b709c2b64df4a2266d71a6bb9edae34b19
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-22 14:53:28 +0900

    [D4] Title: 치즈 도둑, Time: 437 ms, Memory: 6,012 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.cpp
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.cpp
@@ -6,31 +6,19 @@
 using namespace std;
 
 int n;
+
+queue<pair<int, int>> que;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
-
-// 문제의 최대 N 크기 제한에 맞게 전역 배열 선언 (예: N이 최대 100일 경우 105 정도 할당)
-int cheese[105][105];
-bool visited[105][105];
-
-int cheese_block(int day) {
+int cheese_block(vector<vector<int>>& cheese, vector<vector<int>>& visited, int day) {
     int result = 0;
-
-    // 방문 배열 초기화 (동적 할당 없이 기존 메모리 덮어쓰기)
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            visited[i][j] = false;
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (cheese[i][j] > day && !visited[i][j]) {
+            if (cheese[i][j] > day && visited[i][j] != day) {
                 result++;
 
-                queue<pair<int, int>> que;
                 que.push({ i, j });
-                visited[i][j] = true;
+                visited[i][j] = day;
 
                 while (!que.empty()) {
                     auto [x, y] = que.front();
@@ -44,8 +32,8 @@ int cheese_block(int day) {
                             continue;
                         }
 
-                        if (cheese[nx][ny] > day && !visited[nx][ny]) {
-                            visited[nx][ny] = true;
+                        if (cheese[nx][ny] > day && visited[nx][ny] != day) {
+                            visited[nx][ny] = day;
                             que.push({ nx, ny });
                         }
                     }
@@ -57,9 +45,11 @@ int cheese_block(int day) {
     return result;
 }
 
+
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
+
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
 
     int tc;
     cin >> tc;
@@ -68,18 +58,20 @@ int main() {
         cin >> n;
 
         int max_day = 0;
+		vector<vector<int>> cheese(n, vector<int>(n));
+        vector<vector<int>> visited(n, vector<int>(n));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 cin >> cheese[i][j];
-                max_day = max(max_day, cheese[i][j]);
+				max_day = max(max_day, cheese[i][j]);
             }
         }
 
-        int answer = -1;
-        for (int i = 0; i <= max_day; i++) {
-            answer = max(answer, cheese_block(i));
+        int answer = 1;
+        for (int i = 1; i <= max_day; i++) {
+            answer = max(answer, cheese_block(cheese, visited, i));
         }
 
-        cout << "#" << t << " " << answer << "\n";
+        cout << "#" << t << " " << answer << endl;
     }
 }
