d277e74ca44c3c133238294feb02a99b6fe965f6
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-23 16:29:56 +0900

    [D3] Title: 북북서, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/8556. 북북서/북북서.cpp
+++ SWEA/D3/8556. 북북서/북북서.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
+int stack_idx;
+int fw[20];
 
 int main() {
+
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
 
 	int tc;
 	cin >> tc;
@@ -13,25 +17,25 @@ int main() {
 		string s;
 		cin >> s;
 
-		vector<int> fw;
+		stack_idx = 0;
 		for (int i = 0; i < s.size(); ) {
 			if (s[i] == 'n') { // 북쪽
-				fw.push_back(1);
+				fw[stack_idx++] = 1;
 				i += 5;
 			}
 			else { // 서쪽
-				fw.push_back(0);
+				fw[stack_idx++] = 0;
 				i += 4;
 			}
 		}
 
 		long long  num, divide;
-		divide = 1 << (fw.size() - 1); // 분모
-		num = fw.back() ? 0 : 90 * divide; // 분자
+		divide = 1 << (stack_idx - 1); // 분모
+		num = fw[stack_idx - 1] ? 0 : 90 * divide; // 분자
 
 		// 분모를 통분하여 역순 계산
 		long long val = 90 * divide / 2;
-		for (int i = fw.size() - 2; i >= 0; i--) {
+		for (int i = stack_idx - 2; i >= 0; i--) {
 			if (fw[i]) { // 북쪽
 				num -= val;
 			}
