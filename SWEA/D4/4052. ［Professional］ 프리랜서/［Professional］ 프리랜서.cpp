f55a7a1b1f823cd158c6404a515698f0c32550f2
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-28 08:35:38 +0900

    [D4] Title: [Professional] 프리랜서, Time: 19 ms, Memory: 6,148 KB -BaekjoonHub

--- SWEA/D4/4052. ［Professional］ 프리랜서/［Professional］ 프리랜서.cpp
+++ SWEA/D4/4052. ［Professional］ 프리랜서/［Professional］ 프리랜서.cpp
@@ -5,11 +5,13 @@
 using namespace std;
 
 
+/* 작업 정보 구조체 */
 struct Work {
 	int start;
 	int end;
 	int cost;
 
+	// 빠른 작업 순 정렬 로직
 	bool operator<(const Work& other) const {
 		if (start == other.start) {
 			return end < other.end;
@@ -25,16 +27,10 @@ Work work[10002];
 int max_cost[10002] = { 0, };
 
 
-/* 초기값 초기화 */
-void reset(int end) {
-	for (int i = 0; i <= end; i++) {
-		max_cost[i] = 0;
-	}
-}
-
-
 /* 작업 수익 갱신 */
 void cost_calc(int end, int cost) {
+
+	// 현재 작업의 수익이 기존 수익보다 큰 경우에만 갱신
 	if (max_cost[end + 1] < cost) {
 		for (int i = end + 1; i <= m + 1; i++) {
 			max_cost[i] = max(max_cost[i], cost);
@@ -52,19 +48,18 @@ int main() {
 	cin >> tc;
 
 	for (int t = 1; t <= tc; t++) {
-		fill(max_cost, max_cost + 10002, 0);
 		cin >> n >> m;
 
+		// 작업 정보 입력
 		for (int i = 0; i < n; i++) {
 			cin >> work[i].start >> work[i].end >> work[i].cost;
 		}
 
+		// 작업 시작 시간 기준으로 정렬
 		sort(work, work + n);
-		reset(work[0].end);
 
-
-		// dp[i].first = i일까지의 최대 수익
-		// dp[i].second = i일까지의 작업 수행 가능 여부
+		// 수익 계산
+		fill(max_cost, max_cost + m + 2, 0);
 		for (int i = 0; i < n; i++) {			
 			cost_calc(work[i].end, max_cost[work[i].start] + work[i].cost);
 		}
