2de3f67fa535faf9f0500f344d7f1dc3ec7afa65
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-28 08:24:31 +0900

    [D4] Title: [Professional] 프리랜서, Time: 18 ms, Memory: 6,148 KB -BaekjoonHub

--- SWEA/D4/4052. ［Professional］ 프리랜서/［Professional］ 프리랜서.cpp
+++ SWEA/D4/4052. ［Professional］ 프리랜서/［Professional］ 프리랜서.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 
-
 struct Work {
 	int start;
 	int end;
@@ -15,6 +14,7 @@ struct Work {
 		if (start == other.start) {
 			return end < other.end;
 		}
+
 		return start < other.start;
 	}
 };
@@ -22,26 +22,24 @@ struct Work {
 
 int n, m;
 Work work[10002];
-int max_cost[10002] = {0, };
+int max_cost[10002] = { 0, };
 
 
-/* 작업 수익 갱신 */
-void cost_calc(int end, int cost) {
-	// 작업 수행 가능
-	for (int i = end + 1; i <= m + 1; i++) {
-		max_cost[i] = max(max_cost[i], cost);
+/* 초기값 초기화 */
+void reset(int end) {
+	for (int i = 0; i <= end; i++) {
+		max_cost[i] = 0;
 	}
 }
 
 
-/* 디버깅 용 */
-void output() {
-	cout << "--------------------\n";
-	for (int i = 0; i <= m + 1; i++) {
-		cout << max_cost[i] << " ";
+/* 작업 수익 갱신 */
+void cost_calc(int end, int cost) {
+	if (max_cost[end + 1] < cost) {
+		for (int i = end + 1; i <= m + 1; i++) {
+			max_cost[i] = max(max_cost[i], cost);
+		}
 	}
-
-	cout << "\n";
 }
 
 
@@ -62,13 +60,13 @@ int main() {
 		}
 
 		sort(work, work + n);
+		reset(work[0].end);
+
 
 		// dp[i].first = i일까지의 최대 수익
 		// dp[i].second = i일까지의 작업 수행 가능 여부
 		for (int i = 0; i < n; i++) {			
 			cost_calc(work[i].end, max_cost[work[i].start] + work[i].cost);
-
-			//output();
 		}
 
 		cout << "#" << t << " " << max_cost[m + 1] << "\n";
