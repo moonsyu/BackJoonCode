06704ad6eaee2ed877e493d121b11f6cd5415234
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-27 15:29:39 +0900

    [D4] Title: [Professional] CRT, Time: 40 ms, Memory: 5,988 KB -BaekjoonHub

---
+++ SWEA/D4/3951. ［Professional］ CRT/README.md
@@ -0,0 +1,15 @@
+# [D4] [Professional] CRT - 3951 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWH-Dma6EgEDFAUG) 
+
+### 성능 요약
+
+메모리: 5,988 KB, 시간: 40 ms, 코드길이: 650 Bytes
+
+### 제출 일자
+
+2026-07-27 15:29
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3951. ［Professional］ CRT/［Professional］ CRT.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+using namespace std;
+
+int num[10];
+int mul[10];
+
+
+int main() {
+
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int tc;
+	cin >> tc;
+
+	for (int t = 1; t <= tc; t++) {		
+		int n;
+		cin >> n;
+
+		for (int i = 0; i < n; i++) {
+			cin >> num[i] >> mul[i];
+		}
+
+		int answer = 0;
+		bool result = true;
+		while (result) {
+			for (int i = 0; i < n; i++) {
+				if (num[i] < answer) {
+					result = false;
+					num[i] += mul[i];
+				} else if (num[i] > answer) {
+					result = false;
+					answer = num[i];
+				}
+			}
+
+			if (result) {
+				break;
+			} else {
+				result = true;
+			}
+		}
+
+		cout << "#" << t << " " << answer << "\n";
+	}
+}
