be35e54899095f625869aaa03958eb926cae871e
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-27 17:58:00 +0900

    [D4] Title: [Professional] 프리랜서, Time: 46 ms, Memory: 6,148 KB -BaekjoonHub

--- SWEA/D4/4052. ［Professional］ 프리랜서/［Professional］ 프리랜서.cpp
+++ SWEA/D4/4052. ［Professional］ 프리랜서/［Professional］ 프리랜서.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 
 using namespace std;
 
+
+
+struct Work {
+	int start;
+	int end;
+	int cost;
+
+	bool operator<(const Work& other) const {
+		if (start == other.start) {
+			return end < other.end;
+		}
+		return start < other.start;
+	}
+};
+
+
 int n, m;
-vector<int> max_cost(10002, 0);
+Work work[10002];
+int max_cost[10002] = {0, };
+
 
 /* 작업 수익 갱신 */
 void cost_calc(int end, int cost) {
 	// 작업 수행 가능
-	for (int j = end + 1; j <= m + 1; j++) {
-		max_cost[j] = max(max_cost[j], cost);
+	for (int i = end + 1; i <= m + 1; i++) {
+		max_cost[i] = max(max_cost[i], cost);
 	}
-}
-
-
-/* 이전 작업까지의 최대 수익 계산 */
-int search_max_cost(int start) {
-
-	return max_cost[start];
 }
 
 
@@ -30,6 +40,7 @@ void output() {
 	for (int i = 0; i <= m + 1; i++) {
 		cout << max_cost[i] << " ";
 	}
+
 	cout << "\n";
 }
 
@@ -43,32 +54,19 @@ int main() {
 	cin >> tc;
 
 	for (int t = 1; t <= tc; t++) {
-		fill(max_cost.begin(), max_cost.end(), 0);
+		fill(max_cost, max_cost + 10002, 0);
 		cin >> n >> m;
 
-		// 작업 정보 {{start_day, end_day}, cost}
-		vector<pair<pair<int, int>, int>> work(n);
 		for (int i = 0; i < n; i++) {
-			cin >> work[i].first.first >> work[i].first.second >> work[i].second;
+			cin >> work[i].start >> work[i].end >> work[i].cost;
 		}
 
-		// 작업 정보를 시작일 기준으로 정렬
-		sort(work.begin(), work.end());
-
-		//cout << "\n\n";
-		//for (int i = 0; i < n; i++) {
-		//	cout << work[i].first.first << " " << work[i].first.second << " " << work[i].second << "\n";
-		//}
-
+		sort(work, work + n);
 
 		// dp[i].first = i일까지의 최대 수익
 		// dp[i].second = i일까지의 작업 수행 가능 여부
-		for (int i = 0; i < n; i++) {
-			auto [start, end] = work[i].first;
-			int cost = work[i].second;
-			
-			int max_cost = search_max_cost(start);
-			cost_calc(end, max_cost + cost);
+		for (int i = 0; i < n; i++) {			
+			cost_calc(work[i].end, max_cost[work[i].start] + work[i].cost);
 
 			//output();
 		}
