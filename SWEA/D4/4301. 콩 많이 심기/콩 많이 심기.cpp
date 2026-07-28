172af39a5fac78b719e84a81db85f764471e78cb
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-28 09:02:48 +0900

    [D4] Title: 콩 많이 심기, Time: 41 ms, Memory: 6,964 KB -BaekjoonHub

--- SWEA/D4/4301. 콩 많이 심기/콩 많이 심기.cpp
+++ SWEA/D4/4301. 콩 많이 심기/콩 많이 심기.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 
 
 using namespace std;
 
+int n, m;
+bool area[1000][1000];
 
 int dx[4] = { 0, 2, 0, -2 };
 int dy[4] = { 2, 0, -2, 0 };
 
-void four_dir(int x, int y, vector<vector<bool>>& area) {
+
+void reset_area() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			area[i][j] = true;
+		}
+	}
+}
+
+
+void four_dir(int x, int y) {
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
 		int ny = y + dy[i];
 
-		if (nx >= 0 && nx < area.size() && ny >= 0 && ny < area[0].size()) {
+		if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
 			area[nx][ny] = false;
 		}
 	}
@@ -30,19 +40,18 @@ int main() {
 	cin >> tc;
 
 	for (int t = 1; t <= tc; t++) {
-		int n, m;
 		cin >> n >> m;
 
+		reset_area();
+
 		int answer = 0;
-		vector<vector<bool>> area(n, vector<bool>(m, true));
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+			for (int j = 0; j < m; j++) {	
 				if (area[i][j]) {
 					answer++;
-					four_dir(i, j, area);
+					area[i][j] = false;
+					four_dir(i, j);
 				}
-
-				area[i][j] = false;
 			}
 		}
 
