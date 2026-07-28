c08f7bf05b49f831e09a78ee5a2990df63d9327b
moonsyu <54017527+moonsyu@users.noreply.github.com>
2026-07-28 09:04:50 +0900

    [D4] Title: 콩 많이 심기, Time: 44 ms, Memory: 6,964 KB -BaekjoonHub

--- SWEA/D4/4301. 콩 많이 심기/콩 많이 심기.cpp
+++ SWEA/D4/4301. 콩 많이 심기/콩 많이 심기.cpp
@@ -6,10 +6,8 @@ using namespace std;
 int n, m;
 bool area[1000][1000];
 
-int dx[4] = { 0, 2, 0, -2 };
-int dy[4] = { 2, 0, -2, 0 };
 
-
+/* 사용 배열 초기화 */
 void reset_area() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
@@ -19,6 +17,10 @@ void reset_area() {
 }
 
 
+int dx[4] = { 0, 2, 0, -2 };
+int dy[4] = { 2, 0, -2, 0 };
+
+/* 4방향 처리 */
 void four_dir(int x, int y) {
 	for (int i = 0; i < 4; i++) {
 		int nx = x + dx[i];
@@ -42,15 +44,17 @@ int main() {
 	for (int t = 1; t <= tc; t++) {
 		cin >> n >> m;
 
+		// 배열 초기화
 		reset_area();
 
+		// 콩 심기
 		int answer = 0;
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {	
-				if (area[i][j]) {
+			for (int j = 0; j < m; j++) {
+				if (area[i][j]) { // 콩을 심을 수 있을 경우
 					answer++;
-					area[i][j] = false;
-					four_dir(i, j);
+					area[i][j] = false; // 콩 심음 처리
+					four_dir(i, j); // 4방향 처리
 				}
 			}
 		}
